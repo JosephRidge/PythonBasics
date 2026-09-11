7bc56da738e3f61ba3493e92c77891f0f33fb778
JosephRidge <josephridge18@gmail.com>
2026-09-11 08:46:58 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PythonBasics
+
+[Something interesting about variables](https://medium.com/stackademic/python-variables-44f4df890e03)
+[Data Types](https://docs.python.org/3/tutorial/introduction.html)
---
+++ lesson2.py
@@ -0,0 +1,12 @@
+"""
+DATA TYPES: 
+- Tell how we can store things in memory(basically the structure)
+- influenced by ASCII Table(https://www.asciitable.com/)
+- Types: 
+    - Number (Integers & Floating)
+    - Text (String)
+    - Lists (arrays, tuple, set)
+    - Dictionary
+"""
+
+#  Number (Integers & Floating)
