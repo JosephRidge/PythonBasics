e8f3a65c2e106d3ecb7d3643e0e857830051e693
JosephRidge <josephridge18@gmail.com>
2026-09-14 10:48:51 +0300

    update of daa types

---
+++ lesson4.py
@@ -0,0 +1,25 @@
+"""
+- Loops
+- Functions
+- Intro to OOP
+
+"""
+
+output = ""
+# for i in range(10):
+#     print(i)
+
+fruits = ["apple", "mango","pineapples", "kiwi"] 
+
+# for fruit in fruits:
+#     print(fruit) 
+
+for index, fruit in enumerate(fruits):
+    print(f"{index+1}: {fruit}")
+
+
+
+
+print("==" *50)
+print(output)
+print("==" *50)
