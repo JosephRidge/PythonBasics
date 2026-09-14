96aa149154530749cfc372779ce6ac2898fc6c37
JosephRidge <josephridge18@gmail.com>
2026-09-14 10:40:01 +0300

    update of daa types

--- lesson3.py
+++ lesson3.py
@@ -2,9 +2,9 @@
 Operators:
     - Comparison Operators
     - assignment Operators
-    - Bitwise Operators
-    - Arithmetic
-    - Boolean
+    - Bitwise Operators (|, || , & , &&)
+    - Arithmetic (+,-,/,//,**,*)
+    - Boolean (use the Truth tables to get the outcomes or 'and' , 'or', 'not',etc scenarios)
 
 Control flows: 
     - if..else
@@ -69,9 +69,79 @@ output %= output
 
 
 
+"""
+Comparison Operators:
+    - >,<, >=,<=, == 
+    - returns a Boolean value
+"""
+x = 10 
+y = 6
+
+output = x > y
+output = x < y
+output = x == y
+output = x >=y
+output = x <= y
+
+"""
+Boolean operators:
+    - and, or, not 
+"""
+
+x = 10
+y = 6
+z = 4
+output = (x > y) and ( y >= z) # True and True
+output = (x > y) and ( y <= z) # True and False
+
+output = (x > y) or ( y >= z) # True or True
+output = (x > y) or ( y <= z) # True or False
+
+output = not output # negation get the opposite
 
 
+"""
+Control flows: 
+    - if..else
+    - if..elif..else
+    - match 
+"""
 
+color_a = "red"
+color_b = "Red"
+color_c = "RED"
+
+
+# simple if..else
+if( color_a.lower() == color_b.lower()):
+    output = "the colors are the same"
+else:
+    output ="not the same!"
+
+color_b ="brown"
+color_c = "gold"
+
+# if ..elif..else
+if (color_a =="Blue"):
+    output = "sky blue"
+elif(color_b == "BROWN".lower()):
+    output = "brown!".capitalize()
+else:
+    output = "cannot identify the color!"
+
+# mactch case 
+
+color = "Green" 
+
+match(color):
+    case "Green":
+        output = "Green-Light!"
+    case "Red":
+        output = "Stop Car!"
+    case "Orange":
+        output = "Get ready!!"
+    case _: # default outcome if all the cases have not been met  
+        output ="I am lost!"
 
 
 print("="*50)
