34a30b8609a227b0112924e5a97a461e4a1723a6
JosephRidge <josephridge18@gmail.com>
2026-09-14 10:02:14 +0300

    update of daa types

---
+++ lesson3.py
@@ -0,0 +1,84 @@
+"""
+Operators:
+    - Comparison Operators
+    - assignment Operators
+    - Bitwise Operators
+    - Arithmetic
+    - Boolean
+
+Control flows: 
+    - if..else
+    - match
+    - if..elif..else
+"""
+
+"""
+ Arithment operators: 
+    - *, +,-,/,%, //, **
+
+"""
+
+# globale variable
+output = " "
+
+#  %  # used to check whether there exists a remainder in your data , can be used to check whether a number is even or odd
+x = 10 
+y = 2
+y = 3
+x = 115
+y = 3
+output = x % y 
+
+# floor division: you divide two number and if you get a decimal value we truncate  the value
+
+x = 9
+y = 2
+
+output = x/y
+output = x // y # floor division
+
+# power 
+x = 2
+p = 3
+
+output = x ** p 
+
+
+"""
+Assignment Operators:
+   - =, +=, -+, //=, *=, **= , %=, 
+
+"""
+
+x = 10
+y = 11
+new_val =0
+ 
+
+output = x # assignment operator
+new_val += x # this is the same as output = x + output
+output -= x # this is the same as output = output - x
+output = 10
+output *= y # this is the same as output = output * y 
+output /= y
+output //= y
+output = 12
+output **= 2
+output %= output
+
+
+
+
+
+
+
+
+
+print("="*50)
+print(output)
+print("="*50)
+
+
+
+
+
